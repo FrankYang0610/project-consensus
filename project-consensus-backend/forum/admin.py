ec62627db358c9344791448d1a5de474c177c0a7
FrankYang0610 <frankyang0610@icloud.com>
2025-12-26 03:35:47 +0800

    feat: forum admin

---
+++ project-consensus-backend/forum/admin.py
@@ -0,0 +1,250 @@
+"""
+Forum application Django admin configuration.
+
+Provides admin interfaces for ForumPost, ForumPostComment, ForumPostLike,
+and ForumCommentLike models.
+"""
+
+from django.contrib import admin
+from django.utils.html import strip_tags
+
+from .models import ForumPost, ForumPostComment, ForumPostLike, ForumCommentLike
+
+
+@admin.register(ForumPost)
+class ForumPostAdmin(admin.ModelAdmin):
+    """
+    Forum Post Admin Interface
+
+    Provides a comprehensive interface for managing forum posts with filters,
+    search, and bulk actions.
+    """
+    list_display = (
+        "id",
+        "title",
+        "author_name",
+        "tags_display",
+        "likes_count",
+        "comments_count",
+        "is_anonymous",
+        "is_edited",
+        "has_content_warning",
+        "created_at",
+    )
+    list_filter = (
+        "is_anonymous",
+        "is_edited",
+        "has_content_warning",
+        "created_at",
+    )
+    search_fields = ("title", "content", "author__username", "tags")
+    readonly_fields = ("id", "created_at", "likes_count")
+    date_hierarchy = "created_at"
+    list_select_related = ["author"]
+
+    fieldsets = (
+        ("Basic Info", {
+            "fields": ("id", "title", "author", "content"),
+        }),
+        ("Tags", {
+            "fields": ("tags",),
+        }),
+        ("Options", {
+            "fields": ("is_anonymous", "is_edited", "has_content_warning"),
+        }),
+        ("Stats", {
+            "fields": ("likes_count",),
+        }),
+        ("Timestamps", {
+            "fields": ("created_at",),
+        }),
+    )
+
+    def author_name(self, obj):
+        if obj.is_anonymous:
+            return f"{obj.author.username} (Anonymous)"
+        return obj.author.username
+    author_name.short_description = "Author"
+    author_name.admin_order_field = "author__username"
+
+    def tags_display(self, obj):
+        if obj.tags:
+            return ", ".join(obj.tags[:3]) + ("..." if len(obj.tags) > 3 else "")
+        return "-"
+    tags_display.short_description = "Tags"
+
+    def comments_count(self, obj):
+        return obj.comments.count()
+    comments_count.short_description = "Comments"
+
+    actions = ["toggle_content_warning", "clear_content_warning"]
+
+    @admin.action(description="Toggle content warning on selected posts")
+    def toggle_content_warning(self, request, queryset):
+        """Toggle content warning for selected posts"""
+        for post in queryset:
+            post.has_content_warning = not post.has_content_warning
+            post.save(update_fields=["has_content_warning"])
+        self.message_user(
+            request,
+            f"Content warning toggled for {queryset.count()} posts."
+        )
+
+    @admin.action(description="Clear content warning on selected posts")
+    def clear_content_warning(self, request, queryset):
+        """Clear content warning for selected posts"""
+        updated = queryset.update(has_content_warning=False)
+        self.message_user(
+            request,
+            f"Content warning cleared for {updated} posts."
+        )
+
+
+@admin.register(ForumPostComment)
+class ForumPostCommentAdmin(admin.ModelAdmin):
+    """
+    Forum Post Comment Admin Interface
+
+    Provides interface for managing forum comments with soft deletion support.
+    """
+    list_display = (
+        "id",
+        "post_title",
+        "author_name",
+        "reply_to_name",
+        "content_preview",
+        "likes_count",
+        "is_anonymous",
+        "is_deleted",
+        "created_at",
+    )
+    list_filter = (
+        "is_deleted",
+        "is_anonymous",
+        "created_at",
+    )
+    search_fields = ("post__title", "author__username", "content")
+    readonly_fields = ("id", "created_at", "likes_count")
+    date_hierarchy = "created_at"
+    list_select_related = ["author", "post", "reply_to"]
+
+    fieldsets = (
+        ("Basic Info", {
+            "fields": ("id", "post", "author", "content"),
+        }),
+        ("Reply Target", {
+            "fields": ("reply_to",),
+        }),
+        ("Options", {
+            "fields": ("is_anonymous",),
+        }),
+        ("Stats & Status", {
+            "fields": ("likes_count", "is_deleted"),
+        }),
+        ("Timestamps", {
+            "fields": ("created_at",),
+        }),
+    )
+
+    def post_title(self, obj):
+        return obj.post.title[:40] + "..." if len(obj.post.title) > 40 else obj.post.title
+    post_title.short_description = "Post"
+    post_title.admin_order_field = "post__title"
+
+    def author_name(self, obj):
+        if obj.is_anonymous:
+            return f"{obj.author.username} (Anonymous)"
+        return obj.author.username
+    author_name.short_description = "Author"
+    author_name.admin_order_field = "author__username"
+
+    def reply_to_name(self, obj):
+        if obj.reply_to:
+            return obj.reply_to.author.username
+        return "-"
+    reply_to_name.short_description = "Reply To"
+
+    def content_preview(self, obj):
+        if obj.is_deleted:
+            return "[DELETED]"
+        text = strip_tags(obj.content)
+        return text[:50] + "..." if len(text) > 50 else text
+    content_preview.short_description = "Content Preview"
+
+    actions = ["soft_delete_comments", "restore_comments"]
+
+    @admin.action(description="Soft delete selected comments")
+    def soft_delete_comments(self, request, queryset):
+        """Soft delete comments (mark as deleted and clear content)"""
+        updated = queryset.update(is_deleted=True, content="")
+        self.message_user(
+            request,
+            f"{updated} comments were soft deleted."
+        )
+
+    @admin.action(description="Restore selected comments (mark as not deleted)")
+    def restore_comments(self, request, queryset):
+        """Restore deleted comments (note: content cannot be recovered)"""
+        updated = queryset.update(is_deleted=False)
+        self.message_user(
+            request,
+            f"{updated} comments were restored (content was not recovered)."
+        )
+
+
+@admin.register(ForumPostLike)
+class ForumPostLikeAdmin(admin.ModelAdmin):
+    """
+    Forum Post Like Admin Interface
+
+    Provides interface for viewing and managing post likes.
+    """
+    list_display = ("id", "user_name", "post_title", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("user__username", "post__title")
+    readonly_fields = ("created_at",)
+    date_hierarchy = "created_at"
+    list_select_related = ["user", "post"]
+
+    def user_name(self, obj):
+        return obj.user.username
+    user_name.short_description = "User"
+    user_name.admin_order_field = "user__username"
+
+    def post_title(self, obj):
+        return obj.post.title[:40] + "..." if len(obj.post.title) > 40 else obj.post.title
+    post_title.short_description = "Post"
+    post_title.admin_order_field = "post__title"
+
+
+@admin.register(ForumCommentLike)
+class ForumCommentLikeAdmin(admin.ModelAdmin):
+    """
+    Forum Comment Like Admin Interface
+
+    Provides interface for viewing and managing comment likes.
+    """
+    list_display = ("id", "user_name", "comment_author", "comment_preview", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("user__username", "comment__author__username", "comment__content")
+    readonly_fields = ("created_at",)
+    date_hierarchy = "created_at"
+    list_select_related = ["user", "comment", "comment__author"]
+
+    def user_name(self, obj):
+        return obj.user.username
+    user_name.short_description = "User"
+    user_name.admin_order_field = "user__username"
+
+    def comment_author(self, obj):
+        return obj.comment.author.username
+    comment_author.short_description = "Comment Author"
+    comment_author.admin_order_field = "comment__author__username"
+
+    def comment_preview(self, obj):
+        if obj.comment.is_deleted:
+            return "[DELETED]"
+        text = strip_tags(obj.comment.content)
+        return text[:30] + "..." if len(text) > 30 else text
+    comment_preview.short_description = "Comment Preview"
+
